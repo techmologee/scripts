772c0d488740f3195619703b04ccd1bc40b499a1
techmologee <8296114+techmologee@users.noreply.github.com>
2018-04-18 12:22:39 -0400

    Create skypool_extreme-v110.sh

---
+++ skypool_extreme-v110.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
+sleep 3s
+
+THREADS=$(grep -c ^processor /proc/cpuinfo)
+sudo apt-get install unzip
+sudo apt-get install htop
+wget https://github.com/skypool-org/skypool-nimiq-miner/releases/download/v1.1.0/skypool-nimiq-v1.1.0-linux-x64-extreme.zip
+unzip skypool-nimiq-v1.1.0-linux-x64-extreme.zip
+cd skypool-nimiq-v1.1.0-linux-x64-extreme
+
+echo "{
+  \"address\": \"NQ34 A51K 3K6J 4E6Q 07JR VBQA 0JJ3 HC5E AG7U\",
+  \"name\": \"*\",
+  \"thread\": $THREADS,
+  \"percent\": 100,
+  \"server\": \"https://us3.nimiq.skypool.org\"
+}" > config.txt
