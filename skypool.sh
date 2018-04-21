c55e0ba107ee411e9747cd6f005cf63e59db9e38
techmologee <8296114+techmologee@users.noreply.github.com>
2018-04-20 20:49:56 -0400

    Update and rename skypool_extreme-v110.sh to skypool.sh

--- skypool.sh
+++ skypool.sh
@@ -7,15 +7,24 @@ sudo apt-get install unzip
 sudo apt-get install htop
 wget https://github.com/skypool-org/skypool-nimiq-miner/releases/download/v1.1.0/skypool-nimiq-v1.1.0-linux-x64-extreme.zip && unzip skypool-nimiq-v1.1.0-linux-x64-extreme.zip
 sleep 3s
+wget https://github.com/skypool-org/skypool-nimiq-miner/releases/download/v1.1.0/skypool-nimiq-v1.1.0-linux-x64-fast.zip && unzip skypool-nimiq-v1.1.0-linux-x64-fast.zip
 cd skypool-nimiq-v1.1.0-linux-x64-extreme
 echo "{
   \"address\": \"NQ34 A51K 3K6J 4E6Q 07JR VBQA 0JJ3 HC5E AG7U\",
-  \"name\": $NAME,
+  \"name\": \"$NAME\",
   \"thread\": $THREADS,
   \"percent\": 100,
   \"server\": \"https://us3.nimiq.skypool.org\"
 }" > config.txt
-chmod +x skypool-node-client
+cd ~/skypool-nimiq-v1.1.0-linux-x64-fast
+echo "{
+  \"address\": \"NQ34 A51K 3K6J 4E6Q 07JR VBQA 0JJ3 HC5E AG7U\",
+  \"name\": \"$NAME\",
+  \"thread\": $THREADS,
+  \"percent\": 100,
+  \"server\": \"https://us3.nimiq.skypool.org\"
+}" > config.txt
 cd ~
-rm skypool_extreme-v110.sh
+rm skypool-nimiq-v1.1.0-linux-x64-fast.zip
 rm skypool-nimiq-v1.1.0-linux-x64-extreme.zip
+rm skypool.sh
